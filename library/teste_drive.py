a5d5796c32b9dacfa3d12147108c4360e12be46b
Kresley Moura <kre.moura@gmail.com>
2024-09-25 10:50:33 -0300

    Mudança no .gitignore para a secret do google

---
+++ library/teste_drive.py
@@ -0,0 +1,13 @@
+from pydrive.drive import GoogleDrive
+from pydrive.auth import GoogleAuth, ServiceAccountCredentials
+from django.conf import settings
+import os
+
+gauth = GoogleAuth()
+scope = ['https://www.googleapis.com/auth/drive']      
+gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+drive = GoogleDrive(gauth)
+arquivos = drive.ListFile({'q': "'0By6djoRj_4OYN1RMVloyRmttNEU' in parents and trashed=false"}).GetList()
+for arquivo in arquivos:
+    # print(url_base)
+    print(f"nome: {arquivo['title']}, id: {arquivo['id']}")
