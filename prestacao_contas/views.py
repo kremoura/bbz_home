ce25bf34d53987ac41c827c81938c85de8a15468
Kresley Moura <kre.moura@gmail.com>
2024-09-25 09:54:58 -0300

    f

--- prestacao_contas/views.py
+++ prestacao_contas/views.py
@@ -6,7 +6,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
-SCOPES = [
+SCOPES = [ 
     'https://www.googleapis.com/auth/drive.readonly'
 ]
 
