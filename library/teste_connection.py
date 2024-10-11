b0e73209cc964051f14bb49439d20383340c493b
Kresley Moura <kre.moura@gmail.com>
2024-10-11 14:52:17 -0300

    secret odbc

--- library/teste_connection.py
+++ library/teste_connection.py
@@ -1,11 +1,7 @@
 import pyodbc
 
 # Configurações da conexão
-server = 'bbz.mine.nu:1433'
-port = 1433  # Porta de conexão
-database = 'SIGADM'
-username = 'AppConnectPay'
-password = 'a@s67t2h3n@#BB$Z'
+
 
 # Define a string de conexão
 conn_str = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=bbz.mine.nu:1433;DATABASE=SIGADM;UID=AppConnectPay;PWD=a@s67t2h3n@#BB$Z;Timeout=30"
